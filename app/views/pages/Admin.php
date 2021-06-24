3e83293f0e8146bf9824535c14e79dc34ce61901
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-06-24 21:23:38 +0100

    start bulding sidebar Admin page

--- app/views/pages/Admin.php
+++ app/views/pages/Admin.php
@@ -6,12 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
     <title>Blood bank</title>
 </head>
 <body>
+<div class="sidebar">
+    <header>My App</header>
+    <ul>
+        <li><a href="#"><i class="fa fa-qrcode"></i>test1</a></li>
+        <li><a href="#"><i class="fa fa-link"></i>test2</a></li>
+        <li><a href="#"><i class="fa fa-stream"></i>test2</a></li>
+        <li><a href="#"><i class="fa fa-calendar-week"></i>test2</a></li>
+        <li><a href="#"><i class="fa fa-question-circle"></i>test2</a></li>
+        <li><a href="#"><i class="fa fa-sliders-h"></i>test2</a></li>
+        <li><a href="#"><i class="fa fa-envelope"></i>test2</a></li>
+    </ul>
+</div>
+
     
 
-<h1>test</h1>
+
 
 
 
