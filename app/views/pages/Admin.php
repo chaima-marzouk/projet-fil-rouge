b51a688f0a1491cc48fdce5533dff5433ab05f1f
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-06-25 11:38:44 +0100

    add nav

--- app/views/pages/Admin.php
+++ app/views/pages/Admin.php
@@ -13,16 +13,47 @@
 <div class="sidebar">
     <header>LOGO</header>
     <!-- <input type="submit" value="Add new admin"> -->
+    
     <ul>
-        <li><a href="#">test1</a></li>
-        <li><a href="#">test2</a></li>
-        <li><a href="#"><i class="fa fa-stream"></i>test2</a></li>
-        <li><a href="#"><i class="fa fa-calendar-week"></i>test2</a></li>
-        <li><a href="#"><i class="fa fa-question-circle"></i>test2</a></li>
-        <li><a href="#"><i class="fa fa-sliders-h"></i>test2</a></li>
-        <li><a href="#"><i class="fa fa-envelope"></i>test2</a></li>
+         <button>Add new admin</button>
+        <li><a href="#">New post</a></li>
+        <li><a href="#">Pages</a></li>
+        <li><a href="#">Profil</a></li>
+        <!-- <li><a href="#">test2</a></li> -->
+        
     </ul>
 </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style=" margin-left: 249px; margin-top: 20px;">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" style="margin-left:91px;" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+      <span class="oi oi-inbox"></span>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-danger" type="submit">Search</button>
+        <!-- <i class="far fa-comment-dots"></i> -->
+      </form>
+    </div>
+  </div>
+</nav>
 
     
 
