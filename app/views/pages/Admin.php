50149e6fa3baee6733befc71a57fbc6d58a5fc2d
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-06-25 10:09:34 +0100

    styling sidebar

--- app/views/pages/Admin.php
+++ app/views/pages/Admin.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
     <title>Blood bank</title>
 </head>
 <body>
 <div class="sidebar">
-    <header>My App</header>
+    <header>LOGO</header>
+    <!-- <input type="submit" value="Add new admin"> -->
     <ul>
-        <li><a href="#"><i class="fa fa-qrcode"></i>test1</a></li>
-        <li><a href="#"><i class="fa fa-link"></i>test2</a></li>
+        <li><a href="#">test1</a></li>
+        <li><a href="#">test2</a></li>
         <li><a href="#"><i class="fa fa-stream"></i>test2</a></li>
         <li><a href="#"><i class="fa fa-calendar-week"></i>test2</a></li>
         <li><a href="#"><i class="fa fa-question-circle"></i>test2</a></li>
