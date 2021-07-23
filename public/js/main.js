1a816fd54df7c02b7a6e0c030814e7b6ac939f8b
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-07-23 10:21:04 +0100

    stilling the slider menu with JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,14 @@
-//function expression to select elements
+// Function expression to select elements.
 const selectElement = (s) => document.querySelector(s);
-//open the menu on click 
-selectElement('.open').addEvenListener('click', () => {
 
+
+//Open the menu on click
+selectElement('.open').addEventListener('click', () => {
+  selectElement('.nav-list').classList.add('active');
+});
+
+
+//Close the menu on click
+selectElement('.close').addEventListener('click', () => {
+  selectElement('.nav-list').classList.remove('active');
 });
